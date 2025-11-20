179b883f71a86868f91a046cf8024ad92d4e5784
Tejaskakade02 <157232592+Tejaskakade02@users.noreply.github.com>
2025-11-20 18:26:26 +0530

    deleted

--- command
+++
@@ -1,3 +0,0 @@
-docker exec -it 4f8e1a3b1117 bash
-
-ollama pull llama3:8b-instruct-q5_K_M
--- exec.sh
+++
@@ -1,2 +0,0 @@
-docker run --gpus all -it --rm -p 8000:8000 -p 8001:8001   -p 11434:11434 -v "$(pwd)/model_repository:/models" nemo-guardrails-triton:latest
-
